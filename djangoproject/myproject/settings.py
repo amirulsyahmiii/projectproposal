f090db033caa18f054e1eb07455edb8803e442fd
asyraffdanish <129657265+asyraffdanish@users.noreply.github.com>
2023-05-27 22:33:05 +0800

    Update settings.py

--- djangoproject/myproject/settings.py
+++ djangoproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s598u5tib_e29u0j@*622rn@x1y4%p9_7yy6hkdx+og0_5s9l)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'idash99.pythonanywhere.com']
 
 
 # Application definition
